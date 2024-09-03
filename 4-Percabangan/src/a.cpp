7382cf03cce9229f2bca4140ef2293c47851a07e
Ega Fauzan <egafauzan24@gmail.com>
2024-09-03 16:32:12 +0700

    chapter 4 done

---
+++ 4-Percabangan/src/a.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 10;
+
+    if (x > 10) {
+        cout << "nilai x lebih dari 10" << endl;
+    }
+    if (x <= 10) {
+        cout << "nilai x kurang dari atau sama dengan 10" << endl;
+    }
+    if (x >= 9) {
+        cout << "nilai x lebih dari atau sama dengan 9" << endl;
+    }
+}
---
+++ 4-Percabangan/src/e.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 5 * 5;
+
+    if (x == 10) {
+        cout << "kwak" << endl;
+    } else if (x < 25) {
+        cout << "kwek" << endl;
+    } else if (x > 25) {
+        cout << "kwik" << endl;
+    }
+}
