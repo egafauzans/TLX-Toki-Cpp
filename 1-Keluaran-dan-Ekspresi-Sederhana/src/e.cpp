246b535b48d03cae88401bc356122a5707446ea8
Ega Fauzan <egafauzan24@gmail.com>
2024-09-03 13:59:08 +0700

    chapter 1 done

---
+++ 1-Keluaran-dan-Ekspresi-Sederhana/src/e.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    std::cout << "9 kuadrat adalah ";
+    std::cout << "9 * 9" << std::endl;
+    std::cout << "\n\n";
+    std::cout << 42 << std::endl;
+    std::cout << "42" << std::endl;
+    std::cout << 40 + 11 / (5 - 1) << std::endl;
+}
---
+++ 1-Keluaran-dan-Ekspresi-Sederhana/src/g.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+
+int main() {
+    std::cout << "semangat " << (9 * 5) << std::endl;
+}
